977965a324425e26bfecadf0811bfa2fb0c2f1a0
Andre Suchitra <89.andre@gmail.com>
2019-09-08 11:13:17 +0700

    AWS instance

--- client/src/api/backend.js
+++ client/src/api/backend.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
 export default Axios.create({
-  // baseURL: 'http://35.240.144.112',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://ec2-13-229-233-175.ap-southeast-1.compute.amazonaws.com',
+  // baseURL: 'http://localhost:3000',
 });
