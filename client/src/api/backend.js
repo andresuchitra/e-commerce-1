56b95f8dd062064dd6bfccc7beeeea93abeeafd9
Andre Suchitra <89.andre@gmail.com>
2020-06-18 20:51:06 +0700

    updated backend url

--- client/src/api/backend.js
+++ client/src/api/backend.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
 export default Axios.create({
-  baseURL: 'http://ec2-13-229-233-175.ap-southeast-1.compute.amazonaws.com',
-  // baseURL: 'http://localhost:3000',
+  // baseURL: 'http://ec2-13-229-233-175.ap-southeast-1.compute.amazonaws.com',
+  baseURL: process.env.VUE_APP_BACKEND_URL,
 });
